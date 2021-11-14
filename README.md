cfa8466e46a454ad62ae9064a6b2101127b4dca7
Lucira Silva <luciracs0@gmail.com>
2021-11-14 12:43:22 -0300

    feat: add functions to verify mentions and tweet a picture

---
+++ README.md
@@ -0,0 +1,41 @@
+# Simple Twitter Image Bot
+<img src='https://img.shields.io/badge/Python-3.9.6-green' alt='python version'>
+
+## About
+The purpose of this bot is send a picture to a friend using a tweet like that:
+Hi @bot_screen_name send a picture to @friend_screen_name.
+These pictures will be search at a local directory and randomly chosen!
+
+## Technologies
+- Python
+- [Twitter API](https://python-twitter.readthedocs.io/en/latest/index.html)
+
+## Dependencies
+Create the virtual machine
+```
+python3 venv virtual_machine_name
+```
+
+Activate venv
+```
+source venv/bin/activate
+```
+
+Run requirements.txt
+```
+pip install -r requirements.txt
+```
+
+## How to use
+
+Basic Command
+
+![command to use this bot](readme_imgs/bot_command_example.png)
+
+Response
+
+![bot response](readme_imgs/response_bot_example.png)
+
+## About storage pictures
+
+The storage is local and the directory is on gitignore. So you need your own directory of pictures on your computer!
---
+++ bot.py
@@ -0,0 +1,113 @@
+from time import sleep
+from twitter import Api
+from dotenv import load_dotenv
+from os import getenv
+import random
+import os
+from twitter.error import TwitterError
+import runpy
+from typing import Union
+load_dotenv()
+
+SLEEP_TIME = 120
+
+def read_or_write_in_db(method: str = 'r', content='') -> str:
+        with open(getenv('DB_PATH'), method) as db:
+            if method == 'w':
+                db.write(str(content))
+                return
+            return db.read()
+
+class Bot(object):
+    
+    def __init__(self):
+        self.credentials = Api(consumer_key=getenv('CONSUMER_KEY'),
+                      consumer_secret=getenv('CONSUMER_SECRET'),
+                      access_token_key=getenv('ACCESS_TOKEN_KEY'),
+                      access_token_secret=getenv('ACCESS_TOKEN_SECRET'))
+    
+        self.last_saved_mention_id = '' if not read_or_write_in_db() else int(read_or_write_in_db())
+    
+        self.screen_name = self.credentials.VerifyCredentials().screen_name
+    
+    def check_if_exists_new_mentions(self) -> Union[tuple, bool]:
+        
+        current_mentions = self.credentials.GetMentions()
+        
+        if not current_mentions:
+            return False
+        
+        if not self.last_saved_mention_id:
+            
+            self.last_saved_mention_id = current_mentions[0].id
+            
+            read_or_write_in_db(method='w', content=self.last_saved_mention_id)
+        
+            
+        if current_mentions[0].id != self.last_saved_mention_id:
+            for index in range(len(current_mentions)):
+                
+                if (current_mentions[index].id == self.last_saved_mention_id):
+                    
+                    missed_mentions = current_mentions[: index]
+                    
+                    self.last_saved_mention_id = current_mentions[0].id
+                    
+                    read_or_write_in_db(method='w', content=self.last_saved_mention_id)
+                    
+                    return (missed_mentions, True)
+                
+            self.last_saved_mention_id = current_mentions[0].id
+                    
+            read_or_write_in_db(method='w', content=self.last_saved_mention_id)
+                                
+        return False
+        
+    def get_addressed_users_screen_name(self, mentions) -> list:
+        addressed_users = []
+        for index in range(len(mentions)):
+            
+            screen_name = mentions[index].text.split('@')[-1][:-1].strip()
+            if screen_name != self.screen_name:
+                addressed_users.append(screen_name)
+        return addressed_users
+    
+    def tweet_an_image_to_addressed_users(self, missed_mentions):
+
+        addressed_users = self.get_addressed_users_screen_name(missed_mentions)
+        images_name = os.listdir('./images')
+        for user in addressed_users:
+            
+            default_message = 'Hi @{}! Someone sent a picture to you.'.format(user)
+            
+            chosen_image = random.choice(images_name)
+        
+            with open(f'images/{chosen_image}', 'rb') as img:
+                self.credentials.PostUpdate(status=default_message, media=img)
+                
+            print('Tweet done!')
+            sleep(10)
+            
+        return 
+
+if __name__ == '__main__':
+    bot = Bot()
+    
+    while True:
+           
+        try:
+            
+            
+            new_mentions = bot.check_if_exists_new_mentions()
+            
+            if new_mentions:
+                bot.tweet_an_image_to_addressed_users(new_mentions[0]) 
+                
+            print('>> Chillin! <<')
+            sleep(SLEEP_TIME)
+        
+        except TwitterError as e:
+            
+            if e[0]['code'] == 88:
+                
+                runpy.run_path(path_name='bot.py')
